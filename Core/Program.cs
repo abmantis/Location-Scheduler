645a1ae31c311ad1de30c433f1bd2b2dafad09cc
abmantis <amfcalt@gmail>
2011-01-05 16:01:59 +0000

    Added "Core" project

---
+++ Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Core
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("xpto");
+			Console.ReadLine();
+		}
+	}
+}
